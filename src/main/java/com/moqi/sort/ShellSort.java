0633dac796968f4d7d4e1811c7980c4ab37ebab5
moqi1997 <1176323390@qq.com>
2020-03-14 17:49:41 +0800

    希尔排序demo

--- src/main/java/com/moqi/sort/ShellSort.java
+++ src/main/java/com/moqi/sort/ShellSort.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String[] args) {
         int[] arr = {8,9,1,7,2,3,5,4,6,0};
-        shellSort(arr);
+        //shellSort(arr);
+        shellSort1(arr);
     }
     //逐步推导
     public static void shellSort(int[] arr) {
@@ -56,5 +57,24 @@ public class ShellSort {
             }
         }
         System.out.println("希尔排序3轮后=" + Arrays.toString(arr));
+    }
+    public static void shellSort1(int[] arr) {
+        int temp = 0;
+        //希尔排序1
+        //第一轮排序试讲10数据分成5组
+        for (int gap = arr.length / 2; gap > 0 ; gap /= 2) {
+            for (int i = 0;i < arr.length; i++) {
+                //遍历各组中所有的元素（共5组，每组2个元素）步长5
+                for (int j = i - gap; j >= 0 ; j -= gap) {
+                    //如果当前元素大于步长后的元素交换
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+        System.out.println("优化后的希尔排序轮后=" + Arrays.toString(arr));
     }
 }
