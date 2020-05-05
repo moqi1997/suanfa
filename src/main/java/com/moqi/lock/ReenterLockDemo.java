75012b8a71fb12cecf160f7538e2068472b3edb1
moqi1997 <1176323390@qq.com>
2020-05-05 14:40:26 +0800

    重入锁reenterlock lock 实现

--- src/main/java/com/moqi/lock/ReenterLockDemo.java
+++ src/main/java/com/moqi/lock/ReenterLockDemo.java
@@ -1,5 +1,9 @@
 package com.moqi.lock;
 
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author moqi
  * @date 2020/5/4 11:38
@@ -28,10 +32,32 @@ public class ReenterLockDemo {
                 }
             }
         },"t2").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    phone.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"t3").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    phone.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"t4").start();
     }
 }
 
-class Phone{
+class Phone implements Runnable{
     public synchronized void sendSMS () throws Exception{
         System.out.println(Thread.currentThread().getName() + "sendSmS");
         sendEmail();
@@ -40,4 +66,27 @@ class Phone{
         System.out.println(Thread.currentThread().getName() + "sendEmail");
 
     }
+    Lock lock = new ReentrantLock();
+    @Override
+    public void run() {
+        get();
+    }
+    public void get() {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t invoked get()");
+            set();
+        }finally {
+            lock.unlock();
         }
+    }
+
+    public void set() {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t invoked set()");
+        }finally {
+            lock.unlock();
+        }
+    }
+}
